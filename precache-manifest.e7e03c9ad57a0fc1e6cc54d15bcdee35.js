d75475a011319794abbd92009e48e2bdc9f54a23
syedmujahedalih <syedmujahedalih@gmail.com>
2020-05-27 23:52:13 -0700

    Updates

---
+++ precache-manifest.e7e03c9ad57a0fc1e6cc54d15bcdee35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd142058a76fda085455ad68d7f2a2a3",
+    "url": "/syedmujahedalih/covidtracker/index.html"
+  },
+  {
+    "revision": "0c55f725e9460c116a78",
+    "url": "/syedmujahedalih/covidtracker/static/css/main.6a7b0b77.chunk.css"
+  },
+  {
+    "revision": "733f97121ccbb5aac366",
+    "url": "/syedmujahedalih/covidtracker/static/js/2.ef012686.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/syedmujahedalih/covidtracker/static/js/2.ef012686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c55f725e9460c116a78",
+    "url": "/syedmujahedalih/covidtracker/static/js/main.c0024af3.chunk.js"
+  },
+  {
+    "revision": "2868db096027b8bd8357",
+    "url": "/syedmujahedalih/covidtracker/static/js/runtime-main.ce5028e7.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/syedmujahedalih/covidtracker/static/media/covid-19.d7265326.png"
+  }
+]);
